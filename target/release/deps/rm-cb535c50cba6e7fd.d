2ded8842abf73bd1005c2b457e2bf35874435cf8
setokk <tesmtracks@gmail.com>
2024-03-02 19:23:16 +0200

    added gitignore

--- target/release/deps/rm-cb535c50cba6e7fd.d
+++
@@ -1,5 +0,0 @@
-/home/setokk/Projects/rm-rust/target/release/deps/rm-cb535c50cba6e7fd: src/main.rs
-
-/home/setokk/Projects/rm-rust/target/release/deps/rm-cb535c50cba6e7fd.d: src/main.rs
-
-src/main.rs:
--- target/release/rm.d
+++
@@ -1,1 +0,0 @@
-/home/setokk/Projects/rm-rust/target/release/rm: /home/setokk/Projects/rm-rust/src/main.rs
